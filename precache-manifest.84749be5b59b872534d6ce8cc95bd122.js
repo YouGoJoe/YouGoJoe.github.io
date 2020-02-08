093a6b21eaf640abeb9f7ec9e9e777029a722a3f
Joe <joestaller23@gmail.com>
2020-02-08 13:45:01 -0500

    Updates

---
+++ precache-manifest.84749be5b59b872534d6ce8cc95bd122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6ad0f8759fe12140d399e327a293a2",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "5fb2fa16e7b5b14b7db6",
+    "url": "/index.html/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee056a874d4cedd1c7e2",
+    "url": "/index.html/static/js/2.0c745c8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/index.html/static/js/2.0c745c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb2fa16e7b5b14b7db6",
+    "url": "/index.html/static/js/main.a30f67d3.chunk.js"
+  },
+  {
+    "revision": "5c3c4a8e7bcb0f69acde",
+    "url": "/index.html/static/js/runtime-main.ba0a582f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
